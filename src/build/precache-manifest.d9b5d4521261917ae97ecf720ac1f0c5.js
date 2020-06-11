08a22b1b69dbd144f35660dc5fc89a5e5249ac6a
thabisoddx <thabisomoha@gmail.com>
2020-06-11 09:32:41 +0200

    initial commit

---
+++ src/build/precache-manifest.d9b5d4521261917ae97ecf720ac1f0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dffbc2db5544f710acb868945dbc7d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c36022692a15f15933",
+    "url": "/static/css/main.39dac9a6.chunk.css"
+  },
+  {
+    "revision": "13f59e84f18e1fb908b2",
+    "url": "/static/js/2.b2b4b53d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2b4b53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c36022692a15f15933",
+    "url": "/static/js/main.a6c60f9d.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
